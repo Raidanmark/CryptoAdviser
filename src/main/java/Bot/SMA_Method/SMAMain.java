78a7df999c1d37fc716dc32d28dcc3462f1c47f2
Raidanmark <Onidanil@Gmail.com>
2024-10-17 18:16:19 +0200

    AutoDeploy Check #17

--- src/main/java/Bot/SMA_Method/SMAMain.java
+++ src/main/java/Bot/SMA_Method/SMAMain.java
@@ -8,7 +8,7 @@ import Bot.Data.PriceData;
 public class SMAMain {
 
     //Amount of top coins
-    private final Integer TopCoinsListLang = 5;
+    private final Integer TopCoinsListLang = 1;
     //Amount of days logs
     private final Integer DaysAmount = 50;
 
